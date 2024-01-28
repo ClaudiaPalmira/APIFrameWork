eb4bf788caa0546965a83f3c8a5b80242ca35314
7470 <palmira_clau@yahoo.com>
2024-01-28 14:53:14 +0200

    Testare backend

---
+++ src/test/java/ResponseObject/ResponseAccountSuccess.java
@@ -0,0 +1,33 @@
+package ResponseObject;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class ResponseAccountSuccess {
+
+    //invatam despre adnotari JsonProperty numit si alias, porecla
+
+    @JsonProperty("userID")
+    private String userID;
+
+    @JsonProperty("username")
+    private String username;
+
+    //invatam conceptul Compozitie (verisor cu mostenirea) - compozitia construieste obiecte
+
+    @JsonProperty("books")
+    private List<BookObject> books;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<BookObject> getBooks() {
+        return books;
+    }
+}
